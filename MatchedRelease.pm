57b2c2de35ea10401c0d368fef8156e3e3aef509
SteveMalso <the_stylish_wog@hotmail.com>
2018-06-08 23:31:06 +1000

    Update MatchedRelease.pm

--- MatchedRelease.pm
+++ MatchedRelease.pm
@@ -1,6 +1,6 @@
 sub _sendRtorrent {
         my $self = shift;
-        my @script = split $/, `python /home/user/scripts/diskcheck.py '$self->{ti}{torrentName}' '$self->{ti}{torrentSizeInBytes}' '$self->{uploadMethod}{rtLabel}'`;
+        my @script = split $/, `python /home/user/scripts/check.py '$self->{ti}{torrentName}' '$self->{ti}{torrentSizeInBytes}' '$self->{uploadMethod}{rtLabel}'`;
 
         if ($script[0] eq "exit") {
                 exit;
