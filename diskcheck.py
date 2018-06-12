bca8d53bc8706f3fd6b089e0be9fedb15377f382
SteveMalso <the_stylish_wog@hotmail.com>
2018-06-12 19:47:09 +1000

    Update diskcheck.py

--- diskcheck.py
+++ diskcheck.py
@@ -188,5 +188,5 @@ if enable_disk_check == 'yes':
                 xmlrpc('d.erase', tuple([hash]))
                 del torrents[oldest_torrent]
                 available_space = available_space + filesize
-                
+
 print 'finish'
