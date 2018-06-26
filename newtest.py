998f32e265bf1f846afe3b50c42d276f7ab68958
SteveMalso <the_stylish_wog@hotmail.com>
2018-06-26 21:50:08 +1000

    Update newtest.py

--- newtest.py
+++ newtest.py
@@ -3,7 +3,7 @@ if enable_disk_check == 'yes':
         available_space = round(float(disk.f_bsize * disk.f_bavail) / 1024 / 1024 / 1024, 2)
         required_space = torrent_size + 5
         torrents = {}
-		fallback_list = {}
+        fallback_list = {}
         fallback = no
 
         while available_space < required_space:
@@ -18,8 +18,8 @@ if enable_disk_check == 'yes':
                                 label = urllib.unquote(xmlrpc('d.custom1', tuple([hash])))
                                 base_path = xmlrpc('d.base_path', tuple([hash]))
                                 torrents[date] = filesize, ratio, label, base_path, hash
-								
-				if fallback == 'no':
+
+                if fallback == 'no':
                         oldest_torrent = min(torrents)
                         age = (datetime.strptime(datetime.today().strftime('%m/%d/%Y'), '%m/%d/%Y') - datetime.strptime(oldest_torrent.strftime('%m/%d/%Y'), '%m/%d/%Y')).days
                         filesize = torrents[oldest_torrent][0]
@@ -28,25 +28,25 @@ if enable_disk_check == 'yes':
                         base_path = torrents[oldest_torrent][3]
                         hash = torrents[oldest_torrent][4]
 
-				else:
-				        oldest_torrent = min(fallback_list)
-						filesize = fallback_list[oldest_torrent][0]
-						base_path = fallback_list[oldest_torrent][1]
+                else:
+                        oldest_torrent = min(fallback_list)
+                        filesize = fallback_list[oldest_torrent][0]
+                        base_path = fallback_list[oldest_torrent][1]
                         hash = fallback_list[oldest_torrent][2]
 
-				if fallback == 'no':
+                if fallback == 'no':
                         if age < minimum_age or filesize < minimum_filesize or ratio < minimum_ratio or (enable_labels_disk == 'yes' and label not in labels_disk):
 
                                 if (enable_fallback = yes) and (enable_labels_disk == 'yes' and label in labels_disk) and age > minimum_age or filesize > minimum_filesize:
-								        fallback_list[age] = filesize, base_path, hash
+                                        fallback_list[age] = filesize, base_path, hash
 
                                 del torrents[oldest_torrent]
 
                                 if not torrents:
-						        
-								        if enable_fallback = yes and if fallback_list:
-								                fallback = 'yes'
-										        continue
+
+                                        if enable_fallback = yes and if fallback_list:
+                                                fallback = 'yes'
+                                                continue
 
                                         break
 
