c1e3422ff3485ef855b612d7f24a30f135e2d6cd
SteveMalso <the_stylish_wog@hotmail.com>
2018-07-31 18:31:56 +1000

    Update diskcheck.py

--- diskcheck.py
+++ diskcheck.py
@@ -1,33 +1,105 @@
-import sys, os, shutil, PTN, cStringIO as StringIO
+import sys, os, shutil, cStringIO as StringIO
 import xmlrpclib, urllib, urlparse, socket, re
 from urlparse import uses_netloc
 from datetime import datetime
-from imdbpie import Imdb
+
+try:
+        import PTN
+        from imdbpie import Imdb
+except:
+        pass
+
+yes = True
+no = False
+
+include = True
+exclude = False
 
 uses_netloc.append('scgi')
 
-enable_disk_check = 'yes'
-enable_labels_disk = 'yes'
+enable_disk_check = yes
 
 host = 'scgi://127.0.0.1:5000'
 disk = os.statvfs('/')
 
+# The amount of space (in Gigabytes) to be freed on top of the size of the torrent
+buffer = 5
+
+# General Rules
+
+# Filesize in Gigabytes / Age in Days
 minimum_filesize = 5
+minimum_age = 7
 minimum_ratio = 1.2
-minimum_age = 15
 
-labels_disk = ['TV', 'Movies', 'Crap']
+# Fallback Age - Only the age of a torrent must be higher or equal to this number to be deleted - no to disable
+fallback_age = no
 
-labels_imdb = {
-		     "Hollywood Blockbusters" : [7, 50000, 'yes'],
-                     "Bollywood Classics" : [8, 50000, 'no'],
-	      }
+# Fallback Ratio - Only the ratio of a torrent must be higher or equal to this number to be deleted - no to disable
+fallback_ratio = 1.4
+
+# End of General Rules
+
+
+# Tracker Rules will override general rules - Fill to enable
+
+# include: use general rules
+# exclude: exclude tracker
+
+# Value Order - 1. Minimum Filesize (GB) 2. Minimum Age 3. Minimum Ratio 4. Fallback Age 5. Fallback Ratio
+trackers = {}
+
+# Example
+#trackers = {
+#                     "demonoid.pw" : [include],
+#                     "hdme.eu" : [exclude],
+#                     "redacted.ch" : [1, 7, 1.2, no, no],
+#                     "hd-torrents.org" : [3, 5, 1.3, 9, 1.3],
+#                     "privatehd.to" : [5, 6, 1.2, 12, no],
+#                     "apollo.rip" : [2, 5, 1.4, no, 1.8],
+#           }
+
+# Only delete torrents from trackers in your tracker dictionary (yes/no)
+trackers_only = yes
+
+# Add/Exclude labels or set Label Rules - Label Rules will override general/tracker rules - Fill to enable
+
+# include: use general/tracker rules
+# exclude: exclude label
+
+# Value Order - 1. Minimum Filesize (GB) 2. Minimum Age 3. Minimum Ratio 4. Fallback Age 5. Fallback Ratio
+labels = {}
+
+# Example
+#labels = {
+#                     "Trash" : [include],
+#                     "TV" : [exclude],
+#                     "HD" : [1, 5, 1.2, 15, 1.2],
+#         }
+
+# Only delete torrents with labels in your label dictionary (yes/no)
+labels_only = yes
+
+# Exclude torrents without labels (yes/no)
+exclude_unlabelled = no
+
+
+# IMDB Criteria - Fill to enable
+# Value Order - 1. Minimum IMDB Rating 2. Minimum Votes 3. Skip Foreign Movies (yes/no)
+imdb = {}
+
+# Example
+#imdb = {
+#                     "Hollywood Blockbusters" : [7, 80000, yes],
+#                     "Bollywood Classics" : [8, 60000, no],
+#       }
+
 
 class SCGIRequest(object):
 
 	def __init__(self, url):
-		self.url=url
-		self.resp_headers=[]
+		self.url = url
+		self.resp_headers = []
 
 	def __send(self, scgireq):
 		scheme, netloc, path, query, frag = urlparse.urlsplit(self.url)
@@ -69,13 +141,13 @@ class SCGIRequest(object):
 		"Encode string as netstring"
 		return '%d:%s,'%(len(string), string)
 
-	@staticmethod
+        @staticmethod
 	def make_headers(headers):
 		"Make scgi header list"
 		return '\x00'.join(['%s\x00%s'%t for t in headers])+'\x00'
 
 	@staticmethod
-	def add_required_scgi_headers(data, headers=[]):
+	def add_required_scgi_headers(data, headers = []):
 		"Wrap data in an scgi request,\nsee spec at: http://python.ca/scgi/protocol.txt"
 		headers = SCGIRequest.make_headers([('CONTENT_LENGTH', str(len(data))),('SCGI', '1'),] + headers)
 		enc_headers = SCGIRequest.encode_netstring(headers)
@@ -103,11 +175,33 @@ class SCGIRequest(object):
 		return (xmlresp, headers)
 
 
-def xmlrpc(methodname, hash):
-        xmlreq = xmlrpclib.dumps(hash, methodname)
+def imdb_search(torrent_name, minimum_rating, minimum_votes, skip_foreign):
+        imdb = Imdb()
+        torrent_info = PTN.parse(torrent_name)
+
+        try:
+                search = imdb.get_title_ratings(imdb.search_for_title(str(torrent_info['title']) + ' ' + str(torrent_info['year']))[0]['imdb_id'])
+                rating = search['rating']
+                votes = search['ratingCount']
+        except:
+                return
+        else:
+                if rating < minimum_rating or votes < minimum_votes:
+                        print 'exit'
+                        quit()
+
+        if skip_foreign:
+                country = imdb.get_title_versions(imdb.search_for_title(str(torrent_info['title']) + ' ' + str(torrent_info['year']))[0]['imdb_id'])['origins']
+
+                if 'US' not in country:
+                        print 'exit'
+                        quit()
+
+
+def xmlrpc(methodname, params):
+        xmlreq = xmlrpclib.dumps(params, methodname)
         xmlresp = SCGIRequest(host).send(xmlreq)
         return xmlrpclib.loads(xmlresp)[0][0]
-
 
 try:
         torrent_name = str(sys.argv[1])
@@ -118,70 +212,172 @@ except:
         torrent_name = None
         torrent_label = None
 
-if torrent_label in labels_imdb:
-        imdb = Imdb()
-        minimum_rating = labels_imdb[torrent_label][0]
-        minimum_votes = labels_imdb[torrent_label][1]
-        skip_foreign = labels_imdb[torrent_label][2]
-        torrent_info = PTN.parse(torrent_name)
+if torrent_label in imdb:
+        minimum_rating = imdb[torrent_label][0]
+        minimum_votes = imdb[torrent_label][1]
+        skip_foreign = imdb[torrent_label][2]
+        imdb_search(torrent_name, minimum_rating, minimum_votes, skip_foreign)
 
-        try:
-                search = imdb.get_title_ratings(imdb.search_for_title(str(torrent_info['title']) + ' ' + str(torrent_info['year']))[0]['imdb_id'])
-                rating = search['rating']
-                votes = search['ratingCount']
-                country = imdb.get_title_versions(imdb.search_for_title(str(torrent_info['title']) + ' ' + str(torrent_info['year']))[0]['imdb_id'])['origins']
-        except:
-                pass
-        else:
-                if rating < minimum_rating or votes < minimum_votes or (skip_foreign == 'yes' and 'US' not in country):
-                        print 'exit'
-                        quit()
-
-if enable_disk_check == 'yes':
-        torrent_size = round(torrent_size / (1024 * 1024 * 1024.0), 2)
-        available_space = round(float(disk.f_bsize * disk.f_bavail) / 1024 / 1024 / 1024, 2)
-        required_space = torrent_size + 5
+if enable_disk_check:
+        torrent_size /= 1073741824.0
+        downloading = xmlrpc('d.multicall2', ('', 'leeching', 'd.down.total='))
+        available_space = disk.f_bsize * disk.f_bavail / 1073741824.0
+        required_space = torrent_size + buffer
+        min_filesize = minimum_filesize
+        min_age = minimum_age
+        min_ratio = minimum_ratio
+        fb_age = fallback_age
+        fb_ratio = fallback_ratio
         torrents = {}
+        fallback_torrents = {}
+        fallback = False
+        override = False
+
+        if downloading and [0] in downloading:
+
+                try:
+                        available_space -= float(open('autodlcheck.txt').readline())
+                except:
+                        pass
+
+        with open('autodlcheck.txt', 'w+') as textfile:
+                textfile.write(str(torrent_size))
 
         while available_space < required_space:
 
-                if not torrents:
-                        hashes = xmlrpc('download_list', tuple([]))
+                if not torrents and not fallback and fallback_torrents:
+                        fallback = True
 
-                        for hash in hashes:
-                                date = datetime.utcfromtimestamp(xmlrpc('d.creation_date', tuple([hash])))
-                                filesize = round(xmlrpc('d.size_bytes', tuple([hash])) / (1024 * 1024 * 1024.0), 2)
-                                ratio = xmlrpc('d.ratio', tuple([hash])) / 1000.0
-                                label = urllib.unquote(xmlrpc('d.custom1', tuple([hash])))
-                                base_path = xmlrpc('d.base_path', tuple([hash]))
-                                torrents[date] = filesize, ratio, label, base_path, hash
+                if not torrents and not fallback:
+                        completed = xmlrpc('d.multicall2', ('', 'complete', 'd.timestamp.finished=', 'd.custom1=', 't.multicall=,t.url=', 'd.size_bytes=', 'd.ratio=', 'd.base_path=', 'd.hash='))
 
-                oldest_torrent = min(torrents)
-                age = (datetime.strptime(datetime.today().strftime('%m/%d/%Y'), '%m/%d/%Y') - datetime.strptime(oldest_torrent.strftime('%m/%d/%Y'), '%m/%d/%Y')).days
-                filesize = torrents[oldest_torrent][0]
-                ratio = torrents[oldest_torrent][1]
-                label = torrents[oldest_torrent][2]
-                base_path = torrents[oldest_torrent][3]
-                hash = torrents[oldest_torrent][4]
+                        for date, label, tracker, filesize, ratio, path, torrent in completed:
+                                torrents[datetime.utcfromtimestamp(date)] = label, tracker, filesize, ratio, path, torrent
 
-                if age < minimum_age or filesize < minimum_filesize or ratio < minimum_ratio or (enable_labels_disk == 'yes' and label not in labels_disk):
-                        del torrents[oldest_torrent]
+                if not fallback:
+                        oldest_torrent = min(torrents)
 
-                        if not torrents:
-                                break
+                        if override:
+                                override = False
+                                min_filesize = minimum_filesize
+                                min_age = minimum_age
+                                min_ratio = minimum_ratio
+                                fb_age = fallback_age
+                                fb_ratio = fallback_ratio
 
-                        continue
+                        if exclude_unlabelled and not label:
+                                del torrents[oldest_torrent]
 
-                if os.path.isdir(base_path):
-                        shutil.rmtree(base_path)
+                                if not torrents and not fallback_torrents:
+                                        break
+
+                                continue
+
+                        if labels:
+                                label = urllib.unquote(torrents[oldest_torrent][0])
+
+                                if label in labels:
+
+                                        if not labels[label][0]:
+                                                del torrents[oldest_torrent]
+
+                                                if not torrents and not fallback_torrents:
+                                                        break
+
+                                                continue
+
+                                        elif labels[label][0] is not include:
+                                                override = True
+                                                min_filesize = labels[label][0]
+                                                min_age = labels[label][1]
+                                                min_ratio = labels[label][2]
+                                                fb_age = labels[label][3]
+                                                fb_ratio = labels[label][4]
+
+                                elif labels_only:
+                                        del torrents[oldest_torrent]
+
+                                        if not torrents and not fallback_torrents:
+                                                break
+
+                                        continue
+
+                        if trackers and not override:
+                                tracker = torrents[oldest_torrent][1]
+                                rule = [rule for rule in trackers for url in tracker if rule in url[0]]
+
+                                if rule:
+                                        rule = rule[0]
+
+                                        if not trackers[rule][0]:
+                                                del torrents[oldest_torrent]
+
+                                                if not torrents and not fallback_torrents:
+                                                        break
+
+                                                continue
+
+                                        elif trackers[rule][0] is not include:
+                                                override = True
+                                                min_filesize = trackers[rule][0]
+                                                min_age = trackers[rule][1]
+                                                min_ratio = trackers[rule][2]
+                                                fb_age = trackers[rule][3]
+                                                fb_ratio = trackers[rule][4]
+
+                                elif trackers_only:
+                                        del torrents[oldest_torrent]
+
+                                        if not torrents and not fallback_torrents:
+                                                break
+
+                                        continue
+
+                        age = (datetime.now() - oldest_torrent).days
+                        filesize = torrents[oldest_torrent][2] / 1073741824.0
+                        ratio = torrents[oldest_torrent][3] / 1000.0
+                        path = torrents[oldest_torrent][4]
+                        torrent = torrents[oldest_torrent][5]
+
+                        if filesize < min_filesize or age < min_age or ratio < min_ratio:
+
+                                if fb_age is not no and filesize >= min_filesize and age >= fb_age:
+                                        fallback_torrents[oldest_torrent] = path, torrent, filesize
+
+                                elif fb_ratio is not no and filesize >= min_filesize and ratio >= fb_ratio:
+                                        fallback_torrents[oldest_torrent] = path, torrent, filesize
+
+                                del torrents[oldest_torrent]
+
+                                if not torrents:
+
+                                        if fallback_torrents:
+                                                continue
+
+                                        break
+
+                                continue
                 else:
-                        os.remove(base_path)
+                        oldest_torrent = min(fallback_torrents)
+                        path = fallback_torrents[oldest_torrent][0]
+                        torrent = fallback_torrents[oldest_torrent][1]
+                        filesize = fallback_torrents[oldest_torrent][2]
 
-                xmlrpc('d.erase', tuple([hash]))
-                del torrents[oldest_torrent]
-                available_space = available_space + filesize
+                if os.path.isdir(path):
+                        shutil.rmtree(path)
+                else:
+                        os.remove(path)
 
-                if not torrents:
+                xmlrpc('d.erase', tuple([torrent]))
+
+                if not fallback:
+                        del torrents[oldest_torrent]
+                else:
+                        del fallback_torrents[oldest_torrent]
+
+                available_space += filesize
+
+                if not torrents and not fallback_torrents:
                         break
 
 print 'finish'
